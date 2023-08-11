44f393c65114585d8320a02e62eba72a77ae16cc
Doritao <carloshenriqueneves6@gmail.com>
2023-08-11 01:55:03 -0300

    arrumando erro de digitação

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -16,7 +16,7 @@ class User extends Model {
     )
     this.addHook('beforeSave', async(user) => {
         if(user.password) {
-            user.password = await bcrypt.hash(user.password, 10)
+            user.password_hash = await bcrypt.hash(user.password, 10)
         }
     })
   }
